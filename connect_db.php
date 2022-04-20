9ba03854d53710aa86629f65dcb933dbe301f45d
Vasiliy <dvv03122014@gmail.com>
2022-04-20 18:04:29 +0200

    feat(ICE_SHOP) create project

---
+++ connect_db.php
@@ -0,0 +1,5 @@
+<?php
+$dbc = mysqli_connect('vasiliy7.mysql.ukraine.com.ua','vasiliy7_iceb','vasiliy_iceb','vasiliy7_ice3')
+or die('error_mysql');
+
+mysqli_set_charset($dbc,"utf8");
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    require ('connect_db.php');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/style.css">
+    <title>{{title}}</title>
+
+    <script id="title" type="text/x-handlebars-template" ><title>{{title}}</title></script>
+
+    <script id="index" type="text/x-handlebars-template">
+        {{hhhh}}
+        <div>
+            <h1>МОРОЖЕНОЕ</h1>
+            <p>ПОКУПАЙ КАЧЕСТВЕННЫЙ ПРОДУКТ</p>
+            <a href="shop.php" class="shop">ВПЕРЕД</a>
+        </div>
+    </script>
+
+</head>
+<body>
+
+<script src="js/lib/handlebars-v4.0.5.js"></script>
+<script src="js/lib/jquery.js"></script>
+<script src="js/lib/underscore.js"></script>
+<script src="js/controller"></script>
+<script src="js/controller"></script>
+<script src="js/controller"></script>
+</body>
+</html>
